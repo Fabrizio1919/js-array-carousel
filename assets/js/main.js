e32e9893ecfe36b23ae00969bd9387bc619675f6
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-14 16:27:09 +0100

    Add structure button prev and next

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 // Creo un arrey e linko le immagini
 const images = [
-'../assets/img/01.webp',
-'../assets/img/02.webp',
-'../assets/img/03.webp',
-'../assets/img/04.webp',
-'../assets/img/05.webp'
+    '../assets/img/01.webp',
+    '../assets/img/02.webp',
+    '../assets/img/03.webp',
+    '../assets/img/04.webp',
+    '../assets/img/05.webp'
 ]
 
 // Seleziono l'immagine attiva 
@@ -17,8 +17,32 @@ console.log(imagesElement);
 //Loop sulle le immagini e aggiungo un nuovo elemento nella dom img
 for (let i = 0; i < images.length; i++) {
     const imgSrc = images[i];
-    const imagesElement = ` <img class="img-fluid${i === activeImage ? 'active' : ''} "src="${imgSrc}" alt="">`
-    console.log(imagesElement);
-    // InsertAjancetHTML
-    imagesElement.insertAdjacentElement('beforeend', imagesElement)
+    const imgElement = `<img class="img-fluid ${i === activeImage ? 'active' : ''}" src="${imgSrc}" alt="">`
+    console.log(imgElement);
+    // InsertAjacentHTML
+    imagesElement.insertAdjacentHTML('beforeend', imgElement)
 }
+
+// Seleziono slider
+const slideImagesElements = document.querySelectorAll('.slider > .images > img')
+// clic sul button Prev
+const nextEl = document.querySelector('.prev')
+nextEl.addEventListener('click', function () {
+    console.log('cliccato prev');
+    console.log(slideImagesElements); 
+
+
+
+})
+
+// clic sul button Next
+const prevEl = document.querySelector('.next')
+prevEl.addEventListener('click', function () {
+    console.log('cliccato next');
+    console.log(slideImagesElements); 
+
+})
+
+
+
+
