a059cdcc415d34162bac92f03f66ffe748255632
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-14 18:09:58 +0100

    add ./ in const images remove ../

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 // Creo un arrey e linko le immagini
 const images = [
-    '../assets/img/01.webp',
-    '../assets/img/02.webp',
-    '../assets/img/03.webp',
-    '../assets/img/04.webp',
-    '../assets/img/05.webp'
+    "./assets/img/01.webp",
+    "./assets/img/02.webp",
+    "./assets/img/03.webp",
+    "./assets/img/04.webp",
+    "./assets/img/05.webp",
 ]
 
 // Seleziono l'immagine attiva 
